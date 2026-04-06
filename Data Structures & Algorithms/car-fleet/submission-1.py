bb9723448110d984e6e6cab7814ec99da740a76c
JasonJam <57550983+JasonJam0411@users.noreply.github.com>
2026-04-06 16:06:12 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        
+        ans = 0              # 記錄總共有幾個車隊
+        current_slowest = 0  # 記錄目前擋在前面的車隊「抵達所需的時間」
+        
+        # 2. 從最靠近終點的車開始，一台一台往後看
+        for p, s in cars:
+            # 計算這台車單獨開，需要多少時間
+            time = (target - p) / s
+            
+            # 3. 判斷會不會追撞
+            # 如果這台車需要的時間「大於」前面的車隊，代表它比較慢，追不上前車！
+            # 既然追不上，它就會自己帶頭形成一個「新的車隊」
+            if time > current_slowest:
+                ans += 1
+                current_slowest = time  # 更新這組新車隊的時間，讓後面的車來比較
+            
+            # (如果 time <= current_slowest，代表它會追上前車併入車隊，ans 就不用加 1，什麼都不用做)
+                
+        return ans
